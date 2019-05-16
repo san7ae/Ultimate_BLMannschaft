559b97bf87f304cca4cf715c2563bbc30a2cb4ac
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-05-16 19:13:29 +0200

    update scrape

--- main.py
+++ main.py
@@ -1,35 +1,52 @@
-import requests
-from bs4 import BeautifulSoup
+import requests, bs4
+import csv
 
-import pandas as pd
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
 
-headers = {'User-Agent':
-           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
-
-page = "https://www.transfermarkt.co.uk/transfers/transferrekorde/statistik/top/plus/0/galerie/0?saison_id=2000"
+page = "https://www.transfermarkt.co.uk/transfers/transferrekorde/statistik/top/saison_id/2000/plus/0/galerie/0/page/"
 pageTree = requests.get(page, headers=headers)
-pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
+pageSoup = bs4.BeautifulSoup(pageTree.content, 'html.parser')
 
-Players = pageSoup.find_all("a", {"class": "spielprofil_tooltip"})
+outfile = open('main.csv','w', encoding="utf-8", newline='')
+writer = csv.writer(outfile, delimiter=" ")
+writer.writerow(["Players", "Values", "Position", "Nationality"])
 
-#Let's look at the first name in the Players list.
-# print(Players[0].text)
+paging = pageSoup.find("div",{"class":"pager"}).find("ul",{"class":"yiiPager"}).find_all("a")
+start_page = paging[0].text
+last_page = paging[len(paging)-3].text
 
-Values = pageSoup.find_all("td", {"class": "rechts hauptlink"})
 
-# print(Values[0].text)
+totalPlayer = 0
+pages = list(range(1,int(last_page)+1))
+for page in pages:
+    url = 'https://www.transfermarkt.co.uk/transfers/transferrekorde/statistik/top/saison_id/2000/plus/0/galerie/0/page/'+ str(page)
+    html = requests.get(url, headers=headers)
+    soup = bs4.BeautifulSoup(html.content, 'html.parser')
 
-PlayersList = []
-ValuesList = []
+    #Print the page
+    print("======================================================================================")
+    print ('Processing page: %s' %(page))
+    print("======================================================================================")
 
-for i in range(0, 25):
-    PlayersList.append(Players[i].text)
-    ValuesList.append(Values[i].text)
 
-df = pd.DataFrame({"Players": PlayersList, "Values": ValuesList})
+    product_name_list = soup.findAll("tr", {"class":{"even","odd"}})
+    for element in product_name_list:
+        player = element.find("a",{"class":"spielprofil_tooltip"}).text
+        value = element.find("td", {"class": "rechts hauptlink"}).text.replace(u"\u00a3", "").replace("m","").replace(".",",")
+        position = element.select("td")[4].text
 
-df.head()
+        nationality = []
+        nationality1 = element.select("td[class*='zentriert'] img[class*='flaggenrahmen']")[0].attrs["title"]
+        nationality.append(nationality1)
 
-print(df)
 
-df.to_csv("players.csv", index=False)
+
+        print(player)
+        writer.writerow([player, value, position, nationality])
+        totalPlayer+=1
+
+
+outfile.close()
+print("======================================================================================")
+print("TOTAL OF PLAYERS: " + str(totalPlayer))
+print ('Done')
