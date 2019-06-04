5cd385e9237f30577cb7e47cdef6f611393cb304
san7ae <sanjae.me@gmail.com>
2019-06-04 18:59:59 +0200

    Alle erfordertliche daten gescrapt

--- grosschancen-kreiert.py
+++ grosschancen-kreiert.py
@@ -4,14 +4,14 @@ import csv
 headers = {'User-Agent':
            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
-page = "https://www.ligainsider.de/bundesliga/ranking-top-10/torschuesse/saison-2018-2019/"
+page = "https://www.ligainsider.de/bundesliga/ranking-top-10/grosschancen-kreiert/"
 pageTree = requests.get(page, headers=headers)
 pageSoup = bs4.BeautifulSoup(pageTree.content, 'html.parser')
 
 # Write to CSV data
-outfile = open('shotOnTarget.csv','w', encoding="utf-8", newline='')
+outfile = open('grosschancen-kreiert.csv','w', encoding="utf-8", newline='')
 writer = csv.writer(outfile, delimiter=" ")
-writer.writerow(["Name", "Verein", "Position", "Einsatz", "Spielminuten", "Shot On Target"])
+writer.writerow(["Name", "Verein", "Position", "Einsatz", "SpielMinuten", "Grosschancen kreiert"])
 
 
 player_list = pageSoup.findAll("tr")
@@ -28,11 +28,14 @@ for element in player_list:
         spielminutenStr = element.findAll("td", {"class":"text-right"})[1].text
         spielminuten = int(spielminutenStr)
 
-        shotOnaTargetStr = element.findAll("td", {"class": "text-right"})[2].text
-        shotOnaTarget = int(shotOnaTargetStr)
+        grosschancenKreiertStr = element.findAll("td", {"class": "text-right"})[2].text
+        grosschancenKreiert = int(grosschancenKreiertStr)
+
+
+
 
         print(name)
-        writer.writerow([name, verein, position, einsatz, spielminuten, shotOnaTarget])
+        writer.writerow([name, verein, position, einsatz, spielminuten, grosschancenKreiert])
     except:
         print("An exception occurred")
 
